a891d4eb1120f7338d8861be12d54bcace751c67
Ivan Odyntsov <iwan.odintsov@gmail.com>
2020-10-26 16:25:46 +0200

    edit message

---
+++ src/schemas/song-whip.schema.ts
@@ -0,0 +1,78 @@
+
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type SongWhipDocument = SongWhip & Document;
+
+type Link = {
+  link: string;
+  countries: string[];
+};
+
+type Links = {
+  tidal: Link[];
+  itunes: Link[];
+  spotify: Link[];
+  youtubeMusic: Link[];
+};
+
+@Schema()
+export class SongWhip {
+  @Prop()
+  id?: string;
+
+  @Prop({ unique: true })
+  searchTrackUrl: string;
+
+  @Prop()
+  searchCountry: string;
+
+  @Prop()
+  type: string;
+
+  @Prop()
+  sw_id: number;
+
+  @Prop()
+  path: string;
+
+  @Prop()
+  name: string;
+  
+  @Prop()
+  url: string;
+
+  @Prop()
+  sourceUrl: string;
+
+  @Prop()
+  sourceCountry: string;
+
+  @Prop()
+  releaseDate: string;
+
+  @Prop()
+  createdAt: string;
+
+  @Prop()
+  updatedAt: string;
+
+  refreshedAt: string;
+
+  @Prop()
+  image: string;
+
+  @Prop()
+  config: string;
+
+  @Prop()
+  linksCountries: string[];
+
+  @Prop()
+  artists: any[];
+
+  @Prop()
+  links: Links;
+}
+
+export const SongWhipSchema = SchemaFactory.createForClass(SongWhip);
--- src/song-whip/song-whip.module.ts
+++ src/song-whip/song-whip.module.ts
@@ -1,8 +1,18 @@
 import { HttpModule, Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { SongWhip, SongWhipSchema } from 'src/schemas/song-whip.schema';
 import { SongWhipService } from './song-whip.service';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    HttpModule,
+    MongooseModule.forFeature([
+      {
+        name: SongWhip.name,
+        schema: SongWhipSchema,
+      },
+    ]),
+  ],
   providers: [SongWhipService],
   exports: [SongWhipService],
 })
