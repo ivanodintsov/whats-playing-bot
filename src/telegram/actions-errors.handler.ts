33a57cbc1c2e995c66a199dafa42890c7144ff8c
Ivan Odyntsov <ivan@odintsov.me>
2022-05-27 18:45:35 +0300

    feat: improve error handling for actions and commands (#7)
    * feat: add logger to error handlers
    
    * feat: log action error
    
    * feat: remove throw error
    
    * feat: log telegram processor errors
    
    * fix: catch telegram errors

--- src/telegram/actions-errors.handler.ts
+++ src/telegram/actions-errors.handler.ts
@@ -1,46 +1,66 @@
 import { ConfigService } from '@nestjs/config';
 import { Context } from './types';
 import { PREMIUM_REQUIRED } from 'src/spotify/constants';
+import { Logger } from 'src/logger';
 
-export const ActionsErrorsHandler = function(
-  targetClass: any,
-  propertyKey: string,
-  descriptor: TypedPropertyDescriptor<(ctx: Context) => Promise<void>>,
-) {
-  const originalFn = descriptor.value;
+export const ActionsErrorsHandler = function() {
+  return function(
+    targetClass: any,
+    propertyKey: string,
+    descriptor: TypedPropertyDescriptor<(ctx: Context) => Promise<void>>,
+  ) {
+    const originalFn = descriptor.value;
 
-  descriptor.value = async function(ctx: Context) {
-    const appConfig: ConfigService = this.appConfig;
-    try {
-      const response = await originalFn.call(this, ctx);
-      return response;
-    } catch (error) {
-      switch (error.message) {
-        case 'NO_TOKEN':
-          ctx.answerCbQuery(
-            'You should connect Spotify account in a private messages',
-            {
-              url: `t.me/${appConfig.get<string>(
-                'TELEGRAM_BOT_NAME',
-              )}?start=sign_up_pm`,
-            },
-          );
-          break;
+    async function handleError(ctx: Context, error: Error) {
+      const appConfig: ConfigService = this.appConfig;
+      const logger: Logger = this.logger;
 
-        case 'NO_TRACK_URL':
-          ctx.answerCbQuery('Nothing is playing right now ☹️');
-          break;
+      try {
+        switch (error.message) {
+          case 'NO_TOKEN':
+            await ctx.answerCbQuery(
+              'You should connect Spotify account in a private messages',
+              {
+                url: `t.me/${appConfig.get<string>(
+                  'TELEGRAM_BOT_NAME',
+                )}?start=sign_up_pm`,
+              },
+            );
+            break;
 
-        case PREMIUM_REQUIRED:
-          ctx.answerCbQuery('This command requires Spotify Premium ☹️');
-          break;
+          case 'NO_TRACK_URL':
+            await ctx.answerCbQuery('Nothing is playing right now ☹️');
+            break;
 
-        default:
-          ctx.answerCbQuery('No active devices 😒');
-          break;
+          case PREMIUM_REQUIRED:
+            await ctx.answerCbQuery('This command requires Spotify Premium ☹️');
+            break;
+
+          default:
+            logger.error(error.message);
+            await ctx.answerCbQuery('No active devices 😒');
+            break;
+        }
+      } catch (error) {
+        logger.error(error.message);
       }
     }
-  };
 
-  return descriptor;
+    descriptor.value = async function(ctx: Context) {
+      const logger: Logger = this.logger;
+
+      if (!logger) {
+        throw new Error('no Logger dependency');
+      }
+
+      try {
+        const response = await originalFn.call(this, ctx);
+        return response;
+      } catch (error) {
+        handleError.call(this, ctx, error);
+      }
+    };
+
+    return descriptor;
+  };
 };
