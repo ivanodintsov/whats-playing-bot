261adf25ce18e342af13f94e94467413d234d993
Ivan Odyntsov <ivan@odintsov.me>
2022-06-21 12:54:54 +0300

    chore: remove unused type

--- src/telegram/types.d.ts
+++ src/telegram/types.d.ts
@@ -4,16 +4,3 @@ import { TelegramMessage } from './message/message';
 export interface Context extends TelegrafContext {
   domainMessage: TelegramMessage;
 }
-
-export interface CurrentTrack {
-  title: string;
-  name: string;
-  artists: string;
-  url: string;
-  thumb_url: string;
-  thumb_width: number;
-  thumb_height: number;
-  message_text: string;
-  parse_mode: string;
-  uri: string;
-}
